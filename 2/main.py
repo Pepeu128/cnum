b61c6a2ff31a075134d816da0524ccc61c0d8af3
joaopedrocunhaalencar <joaopedrocunhaalencar@gmail.com>
2025-08-09 22:15:18 +0000

    Semana 2

---
+++ 2/main.py
@@ -0,0 +1,43 @@
+
+
+def é_perfeito(n: int) -> bool:
+    if n < 1:
+        return False
+
+    divisores = 0
+    for i in range(1, n):
+        if n % i == 0:
+            divisores += i
+    
+    return divisores == n
+
+def fatorial(n: int) -> int:
+    for n in range(n, 1):
+        if n == 0:
+            return False
+        if (n - 1) == 0:
+            return n
+        else:
+            return n * fatorial(n - 1)
+
+def fatorial(n: int) -> int:
+    if n < 0:
+        raise ValueError ("O número não pode ser negativo")
+    while n > 1:
+        n = n * (n - 1)
+    return n
+
+def main():
+    assert is_perfect(6) == True
+    assert is_perfect(7) == False
+    assert is_perfect(-1) == False
+    assert factorial(5) == 120
+    assert factorial(0) == 1
+    try:
+        factorial(-1)
+    except ValueError as error:
+        assert str(error) == "O número deve ser não negativo."
+
+        
+
+
